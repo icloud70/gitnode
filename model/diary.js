b25bef3aad8050980860ab36ee6a46db17e65ac2
icloud <icloud70@kakao.com>
2022-01-15 10:21:35 +0900

    몽고디비 접속 수정

--- model/diary.js
+++ model/diary.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 //디비 연결
 let url = "mongodb+srv://icloud70:sksms7361@cluster0.tnc8r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.set('useUnifiedTopology', true);
 mongoose.connect(url, {dbName: 'test'}, function(err) {
     console.log('err ::' + err);
 });
